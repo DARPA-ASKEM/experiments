ae4d18687443bc1b2da04dfa6b15ac44ae4d416b
liunelson <nliu@uncharted.software>
2023-06-05 23:49:08 +0000

    Subset dataset for calibration testing

---
+++ python_sandbox/notebooks/milestone_6month_evaluation_2.py
@@ -0,0 +1,14 @@
+# %%[markdown]
+# Author: Nelson Liu
+# 
+# Email: [nliu@uncharted.software](mailto:nliu@uncharted.software)
+
+# %%[markdown]
+# Thin-Thread Integration (TA3)
+#
+# 
+
+# %%
+
+
+df.to_csv('../../thin-thread-examples/milestone_6month/evaluation/ta1/usa-IRDVHN_age.csv')
