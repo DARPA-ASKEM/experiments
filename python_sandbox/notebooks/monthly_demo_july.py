be5af8651f55e0bc3670e332108f2405581945c5
Nelson Liu <nliu@uncharted.software>
2024-06-26 01:48:26 -0400

    Monthly demo datasets

---
+++ python_sandbox/notebooks/monthly_demo_july.py
@@ -0,0 +1,23 @@
+# %%[markdown]
+# Author: Nelson Liu
+# 
+# Email: [nliu@uncharted.software](mailto:nliu@uncharted.software)
+
+# %%[markdown]
+# # Monthly Demo (July 2024)
+# 
+
+
+# %%
+import os
+import json
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# %%
+# Set up dataset
+
+
+
+# %%
