4a205e14c92e22fafbff15466d226c1285d183a0
Nelson Liu <nliu@uncharted.software>
2024-07-02 17:07:59 -0400

    Models to test Funman

---
+++ funman/notebooks/plot_results.py
@@ -0,0 +1,19 @@
+# %%[markdown]
+# Author: Nelson Liu
+# 
+# Email: [nliu@uncharted.software](mailto:nliu@uncharted.software)
+
+# %%[markdown]
+# # Plot FUNMAN Results
+# 
+
+# %%
+import os
+import json
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# %%
+
+
