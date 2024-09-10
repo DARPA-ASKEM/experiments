ccc2a7b22ff9598f059dfe0c116942af099d5b15
Nelson Liu <nliu@uncharted.software>
2024-09-10 14:42:55 -0400

    Testing

---
+++ python_sandbox/notebooks/config_dataset_examples.py
@@ -0,0 +1,26 @@
+# %%[markdown]
+# Author: Nelson Liu
+# 
+# Email: [nliu@uncharted.software](mailto:nliu@uncharted.software)
+
+# %%
+import json
+import pandas as pd
+
+
+
+# %%[markdown]
+# # Generate Configure with Dataset Examples
+# 
+# ##  1. Time-series Example
+# 
+# * Start with a configured SIDARTHE model
+# * Run `pyciemss.sample` to get DataFrame CSV output
+
+# %%
+model_sidarthe = 
+
+
+
+# %%[markdown]
+# ## 2. Stratified Parameter Example
