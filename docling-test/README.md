708315baf242513f1ea82ac6bde6157b337db567
Jaehwan Ryu <jryu@uncharted.software>
2025-03-05 09:26:45 -0500

    table extract experiment outputs

--- docling-test/README.md
+++ docling-test/README.md
@@ -2,5 +2,10 @@
 Testing ground for the Docling processing pipeline (https://github.com/DS4SD/docling)
 
 ```
-pip install docling
+# Setup venv (recommended)
+python3 -m venv venv
+source venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
 ```
--- docling-test/simple.py
+++ docling-test/simple.py
@@ -4,7 +4,8 @@ from docling.document_converter import DocumentConverter
 ################################################################################
 # Very basic vanilla test for PDF extractions
 ################################################################################
-source = "/Users/dchang/workspace/askem/pdfs/SIDARTHE_Giordano2020.pdf"
+# source = "/Users/dchang/workspace/askem/pdfs/SIDARTHE_Giordano2020.pdf"
+source = "pdfs/SIDARTHE paper.pdf"
 
 converter = DocumentConverter()
 result = converter.convert(source)
